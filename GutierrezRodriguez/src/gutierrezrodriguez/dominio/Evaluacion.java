39276d22a5d82a8ff43385378a7c8ffd12956a4c
GUTI-PC\Matías <gutierrez.matias@outlook.com>
2016-11-20 13:13:49 -0300

    Cambios en clase Evaluacion.java

--- GutierrezRodriguez/src/gutierrezrodriguez/dominio/Evaluacion.java
+++ GutierrezRodriguez/src/gutierrezrodriguez/dominio/Evaluacion.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gutierrezrodriguez.dominio;
 
-/**
- *
- * @author Matías
- */
 public class Evaluacion {
+    private int estrellas;
+    private String resena;
+    private String nombre;
     
+    public Evaluacion(){
+        this.estrellas = 0;
+        this.resena = "";
+    }
+    
+    public Evaluacion(int estrellas, String resena){
+        this.estrellas = estrellas;
+        this.resena = resena;
+    }
+    public int getEstrellas() {
+        return estrellas;
+    }
+
+    public void setEstrellas(int estrellas) {
+        this.estrellas = estrellas;
+    }
+
+    public String getResena() {
+        return resena;
+    }
+
+    public void setResena(String resena) {
+        this.resena = resena;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
