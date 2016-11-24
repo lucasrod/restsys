00ae72bd3be3d1983c47863b5901f7f2cbe0a185
GUTI-PC\Matías <gutierrez.matias@outlook.com>
2016-11-23 22:47:01 -0300

    Datos de prueba

--- GutierrezRodriguez/src/gutierrezrodriguez/Inicio.java
+++ GutierrezRodriguez/src/gutierrezrodriguez/Inicio.java
@@ -2,13 +2,13 @@ package gutierrezrodriguez;
 
 import gutierrezrodriguez.dominio.*;
 import gutierrezrodriguez.interfaz.Frame;
-import java.util.ArrayList;
 
 public class Inicio {
 
     public static void main(String[] args) {   
 
         Sistema sistema = new Sistema();
+        sistema.cargarDatosDePrueba();
         Frame ventana = new Frame(sistema);
         ventana.setVisible(true);
     }
